a9e1fc308643e5ea20a490fb9771afae2b362883
a-reznikov <a.reznnikov@gmail.com>
2023-01-24 17:13:54 +0300

    feat: implement solushion

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = String(expr).split("");
+    let tenCharacters = [];
+    let letterConvert = [];
+    let start = 0;
+    let end = 10;
+    while ((tenCharacters.length * 10) < array.length) {
+        let tenCharactersSlice = array.slice(start, end);
+        let codeLatter = '';
+            for (let step = 0; step < 10;) {
+                if ((tenCharactersSlice[step] + tenCharactersSlice[step + 1]) == 10) {
+                    codeLatter += ".";
+                    // console.log((tenCharactersSlice[step] + tenCharactersSlice[step + 1]) + "it's dots");
+                } else if ((tenCharactersSlice[step] + tenCharactersSlice[step + 1]) == '11') {
+                    codeLatter += "-";
+                    // console.log((tenCharactersSlice[step] + tenCharactersSlice[step + 1]) + "it's dashes");
+                } else {
+                    // console.log(tenCharactersSlice[step] + tenCharactersSlice[step + 1]);
+                }
+                step += 2;
+            }
+        // console.log(codeLatter);
+        tenCharacters.push(tenCharactersSlice);
+        letterConvert.push(codeLatter);
+        start +=10;
+        end +=10;
+    }
+    // console.log(letterConvert);
+    let secretWords = '';
+    for (letter in letterConvert) {
+        if (letterConvert[letter] in MORSE_TABLE) {
+            secretWords += MORSE_TABLE[letterConvert[letter]];
+            // console.log(MORSE_TABLE[letterConvert[letter]]);
+        } else {
+            secretWords += " ";
+        }
+    }
+    return secretWords;
 }
 
 module.exports = {
